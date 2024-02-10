faa62f1596be11760a2ace5e526b030b7feb77f0
ayusha2806 <anandayush2806@gmail.com>
2024-02-10 17:29:51 +0530

    Nextjs Starting

---
+++ pages/aboutus.js
@@ -0,0 +1,7 @@
+
+
+function AboutUsPage(){
+    return <h1> The Aboutus Page</h1>
+}
+
+export default AboutUsPage;
---
+++ pages/news.js
@@ -0,0 +1,7 @@
+
+
+function NewsPage(){
+    return <h1> The NewsPage</h1>
+}
+
+export default NewsPage;
