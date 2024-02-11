df7a93288a01e3842935e29745534d222f11687d
ayusha2806 <anandayush2806@gmail.com>
2024-02-11 12:12:50 +0530

    Final touch to About US page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,7 +1,27 @@
 
+import Link from 'next/link';
 
-function AboutUsPage(){
-    return <h1> The Aboutus Page</h1>
-}
+const AboutUsPage = () => {
+  const developers = [
+    { id: '1', name: 'Yash' },
+    { id: '2', name: 'Vaibhav' },
+    { id: '3', name: 'Suresh' }
+  ];
+
+  return (
+    <div>
+      <h1>Meet Our Team</h1>
+      <ul>
+        {developers.map(developer => (
+          <li key={developer.id}>
+            <Link href={`/aboutus/${developer.id}`}>
+              <span>{developer.name}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default AboutUsPage;
